b2460e2d62c1cd33a60d8a0c7b671e462532d528
Alexey Vasilev <escantor@gmail.com>
2019-04-12 15:51:34 +0300

    Lesson-1 first commit

---
+++ lesson-1/star.py
@@ -0,0 +1,42 @@
+import time
+import curses
+import asyncio
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+def draw(canvas):
+    while True:
+        row, column = (5, 20)
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.border()
+        curses.curs_set(False)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
