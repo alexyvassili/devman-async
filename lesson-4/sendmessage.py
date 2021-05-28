762cc5d05640e9bf1fb6d98f83f91549ecc16e06
Alexey Vasilev <escantor@gmail.com>
2021-05-28 12:16:18 +0300

    Refactoring logging

--- lesson-4/sendmessage.py
+++ lesson-4/sendmessage.py
@@ -10,29 +10,7 @@ args = get_config()
 ACCESS_LOG_FILE = 'sendmessage.log'
 LOGGING_FORMAT = '[%(asctime)s] %(levelname).1s %(message)s'
 
-
-def create_log_files_if_not_exist():
-    if not os.path.exists(ACCESS_LOG_FILE):
-        os.mknod(ACCESS_LOG_FILE)
-
-
-def setup_loggers():
-    logFormatter = logging.Formatter(LOGGING_FORMAT)
-    rootLogger = logging.getLogger()
-
-    message_file_handler = logging.FileHandler(ACCESS_LOG_FILE)
-    message_file_handler.setFormatter(logFormatter)
-    rootLogger.addHandler(message_file_handler)
-
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(consoleHandler)
-
-    rootLogger.setLevel(logging.DEBUG)
-
-
-create_log_files_if_not_exist()
-setup_loggers()
+log = logging.getLogger('send_message')
 
 
 def get_token():
@@ -49,9 +27,9 @@ def get_token():
 async def sign_up(reader, writer):
     # Enter preferred nickname below
     data = await read_response(reader)
-    logging.debug(data.decode())
+    log.debug(data.decode())
     nickname = input()
-    logging.debug("Nickname: %s", nickname)
+    log.debug("Nickname: %s", nickname)
     writer.write(f"{nickname}\n".encode())
     # account data
     data = await read_response(reader)
@@ -63,7 +41,7 @@ async def login(reader, writer):
     token = get_token()
     data = await read_response(reader)
     # Enter your personal hash or leave it empty...
-    logging.debug(data.decode())
+    log.debug(data.decode())
     if token:
         writer.write(f"{token}\n".encode())
         # Return nickname and hash
@@ -72,12 +50,12 @@ async def login(reader, writer):
         # Token may be invalid and we have empty account
         # So we try create user
         if not account:
-            logging.debug("Your TOKEN is invalid! Let's try to create new user!")
+            log.debug("Your TOKEN is invalid! Let's try to create new user!")
             account = await sign_up(reader, writer)
     else:
         writer.write(b"\n")
         account = await sign_up(reader, writer)
-    logging.debug("Logged in as: %s", account["nickname"])
+    log.debug("Logged in as: %s", account["nickname"])
 
 
 async def read_response(reader):
@@ -87,26 +65,26 @@ async def read_response(reader):
             data = await asyncio.wait_for(reader.readline(), timeout=3.0)
         except (ConnectionRefusedError, ConnectionResetError, ConnectionError, asyncio.TimeoutError) as e:
             if retry_timeout:
-                logging.debug("Нет соединения. Повторная попытка через %s сек.\n", retry_timeout)
+                log.debug("Нет соединения. Повторная попытка через %s сек.\n", retry_timeout)
             else:
-                logging.debug("Нет соединения. Повторная попытка.\n")
+                log.debug("Нет соединения. Повторная попытка.\n")
             await asyncio.sleep(retry_timeout)
             retry_timeout = 3
             continue
         else:
             if retry_timeout:
-                logging.debug("Установлено соединение.\n")
+                log.debug("Установлено соединение.\n")
                 retry_timeout = 0
 
         return data
 
 
 async def send_message(reader, writer, message):
-    logging.debug("Sending message: %s", message)
+    log.debug("Sending message: %s", message)
     # NB:  мне не удалось сломать скрипт, передавая \n в середине сообщения или ника
     writer.write(f"{message}\n\n".encode())
     data = await read_response(reader)
-    logging.debug(data.decode())
+    log.debug(data.decode())
 
 
 async def send_many_messages(reader, writer):
@@ -121,9 +99,9 @@ async def send_many_messages(reader, writer):
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(args.server, args.write_port)
     await login(reader, writer)
-    logging.debug("Start send message. Press Ctrl-C to quit")
+    log.debug("Start send message. Press Ctrl-C to quit")
     data = await read_response(reader)
-    logging.debug(data.decode())
+    log.debug(data.decode())
 
     if args.message:
         # Send message and exit
@@ -131,8 +109,23 @@ async def tcp_echo_client():
     else:
         await send_many_messages(reader, writer)
 
-    logging.debug('Close the connection')
+    log.debug('Close the connection')
     writer.close()
 
 
-asyncio.run(tcp_echo_client())
+if __name__ == '__main__':
+    if not os.path.exists(ACCESS_LOG_FILE):
+        os.mknod(ACCESS_LOG_FILE)
+
+    logFormatter = logging.Formatter(LOGGING_FORMAT)
+
+    message_file_handler = logging.FileHandler(ACCESS_LOG_FILE)
+    message_file_handler.setFormatter(logFormatter)
+    log.addHandler(message_file_handler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    log.addHandler(consoleHandler)
+
+    log.setLevel(logging.DEBUG)
+    asyncio.run(tcp_echo_client())
